a968f3957472a5ddda5550542d37c6fcb204134a
Utae <utae92@gmail.com>
2019-07-04 18:11:59 +0900

    change navigation from react-native-navigation to react-navigation

--- android/app/src/main/java/com/triplus/MainActivity.java
+++ android/app/src/main/java/com/triplus/MainActivity.java
@@ -1,9 +1,34 @@
 package com.triplus;
 
-import com.reactnativenavigation.NavigationActivity;
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen;
 
-public class MainActivity extends NavigationActivity {
+public class MainActivity extends ReactActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected String getMainComponentName() {
+        return "Triplus";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
 }
 
 //package com.triplus;
--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-/**
- * @format
- */
+import { AppRegistry } from 'react-native';
 
-import { Navigation } from "react-native-navigation";
-import App from './App';
+import App from "./App"
 
-Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
-
-Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-        root: {
-            component: {
-                name: "navigation.playground.WelcomeScreen"
-            }
-        }
-    });
-});
+AppRegistry.registerComponent('Triplus', () => App);
