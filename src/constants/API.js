74a4ccee50ca21600dce25d982a4f64a1fb49f00
Utae <utae92@gmail.com>
2019-07-26 17:03:43 +0900

    add some API

--- src/constants/API.js
+++ src/constants/API.js
@@ -6,3 +6,9 @@ export const registration = SERVER_URL + '/rest-auth/registration';
 export const logout = SERVER_URL + '/rest-auth/logout';
 
 export const CITY_LIST = SERVER_URL + '/api/v1/city';
+
+export const TRIP_INFO_LIST = SERVER_URL + '/api/v1/trip_info';
+export const TRIP_INFO_DETAIL = (pk) => (SERVER_URL + '/api/v1/trip_info/'+ pk + '/details');
+
+export const TRIP_PACKAGE_LIST = SERVER_URL + '/api/v1/trip_package';
+export const TRIP_INFO_DETAIL = (pk) => (SERVER_URL + '/api/v1/trip_package/'+ pk + '/details');
