8030ade513f8f859a85fd08341c5ddc931e3b564
Utae <utae92@gmail.com>
2019-07-29 16:55:04 +0900

    add assets

--- assets/image/Header/index.js
+++ assets/image/Header/index.js
@@ -2,3 +2,4 @@ export const drawerBtnBlack = require('./navBtn_black.png');
 export const drawerBtnWhite = require('./navBtn_white.png');
 export const backBtnBlack = require('./backBtn_black.png');
 export const backBtnWhite = require('./backBtn_white.png');
+export const see_all = require('./see_all.png');
